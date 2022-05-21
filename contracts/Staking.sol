06267d4136dca2e08c8ab1b0b7a58376f0c19d70
Gor Karapetyan <gor.karapetya@exio.tech>
2022-05-21 22:01:14 +0400

    add safeERC20

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Staking {
-    using SafeERC20 for sweetToken;
+    using SafeERC20 for IERC20;
 
     uint256 public constant BLOCKS_PER_DAY = 10;
 
