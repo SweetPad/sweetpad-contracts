b941af8dc6eaec347d812ac19a167cdbfef3c920
Gor Karapetyan <gor.karapetya@exio.tech>
2022-05-31 00:15:22 +0400

    remove console

--- contracts/SweetpadFreezing.sol
+++ contracts/SweetpadFreezing.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./interfaces/ISweetpadFreezing.sol";
-import "hardhat/console.sol";
 
 /**
  * @title SweetpadFreezing
