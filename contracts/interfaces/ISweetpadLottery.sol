de475ad6a7b757b968595766032f6aa9442433d2
Gor Karapetyan <gor.karapetya@exio.tech>
2022-08-15 14:07:41 +0400

    fix versions, comment unneaded code

--- contracts/interfaces/ISweetpadLottery.sol
+++ contracts/interfaces/ISweetpadLottery.sol
@@ -2,21 +2,19 @@
 pragma solidity ^0.8.7;
 
 interface ISweetpadLottery {
-
     //-------------------------------------------------------------------------
     // VIEW FUNCTIONS
     //-------------------------------------------------------------------------
 
-    function getMaxRange() external view returns(uint32);
+    function getMaxRange() external view returns (uint32);
 
     //-------------------------------------------------------------------------
-    // STATE MODIFYING FUNCTIONS 
+    // STATE MODIFYING FUNCTIONS
     //-------------------------------------------------------------------------
 
     function numbersDrawn(
         uint256 _lotteryId,
-        bytes32 _requestId, 
+        bytes32 _requestId,
         uint256 _randomNumber
-    ) 
-        external;
+    ) external;
 }
